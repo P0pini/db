869b55617e6daf50dd80b8acf67d58eb6462a401
P0pini <popiksonek@gmail.com>
2025-05-06 14:41:07 +0200

    db

---
+++ src/main/java/org/example/database/DatabaseConnection.java
@@ -0,0 +1,32 @@
+package org.example.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private Connection connection;
+
+    public void connect(String dbPath){
+        try {
+            connection = DriverManager.getConnection("jdbc:sqlite:" + dbPath);
+        } catch(SQLException e){
+            //Wyrzucamy RuntimeException, bo po błędzie połączenia z bazą
+            //dalsza część programu nie ma sensu i możemy zokończyć z błędem
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void disconnect(){
+        if(connection == null)
+            return;
+        try {
+            connection.close();
+        }catch (SQLException e){
+            System.err.println(e.getMessage());
+        }
+    }
+    public Connection getConnection(){
+        return connection;
+    }
+}
